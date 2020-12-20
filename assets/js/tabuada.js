d89a29c22c3f24d05cc62820feea314f62a9bb0d
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2020-12-20 17:25:00 -0300

    Implementado código para tabuada em JavaScript

--- assets/js/tabuada.js
+++ assets/js/tabuada.js
@@ -1,17 +1,17 @@
 function calculaTabuada() {
 
 
+let tabuada = document.querySelector("#tabuada tbody");
+
+
+let valorA = parseInt(document.querySelector("#valorA").value);
 
 
 
-let valorA = 99;
+tabuada.innerHTML = '';
 
 
-
-
-
-
-for(let valorB = 0; valorB <=10; valorB++){
+for(let valorB = 0; valorB <= 10; valorB++) {
 
 
 
@@ -19,14 +19,31 @@ let resultado = valorA * valorB;
 
 
 
+let template = `
 
+<td>${valorA}</td>
+<td>x</td>
+<td>${valorB}</td>
+<td>=</td>
+<td>${resultado}</td>
+
+
+`;
+
+let tr = document.createElement('tr');
+
+tr.innerHTML = template;
+
+tabuada.append(tr);
 
 }
 
 
 
-
-
-
-
 }
+
+
+    calculaTabuada();
+
+
+    document.querySelector("#valorA").addEventListener('change', calculaTabuada);
