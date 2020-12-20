eed8488d4500b75c8f0797f75821729973a92cb6
gilbertobreyerjunior <gilbertobreyer.jr@gmail.com>
2020-12-20 16:56:34 -0300

    Corrigido layout HTML e iniciado JavaScript

---
+++ assets/js/tabuada.js
@@ -0,0 +1,32 @@
+function calculaTabuada() {
+
+
+
+
+
+let valorA = 99;
+
+
+
+
+
+
+for(let valorB = 0; valorB <=10; valorB++){
+
+
+
+let resultado = valorA * valorB;
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
